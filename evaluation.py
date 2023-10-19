235017d77d28354ea4f4f8105e370780503dcf3f
lcmmichielsen <lieke.michielsen@hotmail.com>
2023-10-19 10:16:10 +0200

    Add DL models

--- evaluation.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug 26 12:06:00 2022
-
-@author: lcmmichielsen
-"""
-
-## Script with custom evaluation functions
-## Add some metrics that emphasize the differences here?
